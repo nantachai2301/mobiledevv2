42979a1bf5927425b6e062afaf96a92ac89bcdac
nantachai2301 <jayza55255@gmail.com>
2023-03-14 20:09:47 +0700

    14/3/2566

--- react-se-booking/src/pages/MailList.js
+++ react-se-booking/src/pages/MailList.js
@@ -1,39 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const MailList = () => {
   return (
-    <div className="mailList">
-      <div className="mailContainer">
-        <div className="mailTitle">
-          <center>
-            <h1> Save time, save money</h1>
-          </center>
-          <center>
-            <p className="title" >
-              Sign up and we'll send the best deais to you
-            </p>
-          </center>
-        </div>
-        <div className="input">
-          <center>
-            <input
-              type="email"
-              placeholder="Your Email"
-              className="inputemail"
-            />
-
-            <button className="btn">
-              Subscribe
-            </button>
-          </center>
-        </div>
-
-        <div className="body">
-          <div></div>
-        </div>
+    <div className="maiList">
+      <div className="maliContainer">
+        <h1> Save time , Save money!</h1>
+        <p> Sing up and we'll send the best deatals to you</p>
+        <input type="search" id="search" placeholder="Your Email" />
+        <button className="btn">Subscribe</button>
       </div>
     </div>
   );
-}
+};
 
-export default MailList
+export default MailList;
